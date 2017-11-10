291e3b9cace2b29e4902eb6811e5672b9db39f79
Benjamin Calef <benjamin.calef@caudalie.com>
2017-11-10 12:14:10 +0100

    install new theme and grunt

---
+++ dev/tools/grunt/configs/theme.smile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+    my_project_theme: {
+        area: 'frontend',
+        name: 'Training/custom',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
